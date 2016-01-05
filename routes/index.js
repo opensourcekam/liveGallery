f4eea7df8adc0e6af5879bc0fe12a331cb134858
RobinsonKameron <ka12@umbc.edu>
2016-01-05 00:12:52 -0500

    Added routes to speed up loading on client

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,40 @@ var dec30 = videos.dec30_mp4;
 var dec31 = videos.dec31_mp4;
 var dec1 = videos.dec1_mp4;
 var dec2 = videos.dec2_mp4;
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Live Gallery',
-        dec29: dec29,
-        dec30: dec30,
-        dec31: dec31,
-        dec1: dec1,
+        dec29: dec29
+    });
+});
+
+/* GET 30 */
+router.get('/30', function (req, res) {
+    res.render('30', {
+        title: 'Live Gallery',
+        dec30: dec30
+    });
+});
+/* GET 31 */
+router.get('/31', function (req, res) {
+    res.render('31', {
+        title: 'Live Gallery',
+        dec31: dec31
+    });
+});
+/* GET 1 */
+router.get('/1', function (req, res) {
+    res.render('1', {
+        title: 'Live Gallery',
+        dec1: dec1
+    });
+});
+/* GET 2 */
+router.get('/2', function (req, res) {
+    res.render('2', {
+        title: 'Live Gallery',
         dec2: dec2
     });
 });
